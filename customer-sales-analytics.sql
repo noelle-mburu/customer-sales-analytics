296d36f18acaa46d092dbd80dfb509ad82f8992e
Noelle Mburu <noellemburu@gmail.com>
2026-03-06 23:57:14 +0300

    Updated queries

--- customer-sales-analytics.sql
+++ customer-sales-analytics.sql
@@ -80,4 +80,29 @@ inner join olist.order_items oi on s.seller_id = oi.seller_id
 group by s.seller_id, s.seller_city, s.seller_state
 order by total_revenue_per_seller desc
 limit 10;
-	
+
+-- 5. Delivery — What is the average delivery time? Are orders being delivered on time?
+select * from orders o;
+
+select 
+	ROUND(AVG(DATE_PART('day',o.order_delivered_customer_date - o.order_purchase_timestamp))::numeric,1) as average_delivery_days
+from olist.orders o
+where o.order_status = 'delivered';
+
+-- 6. Payments — What payment methods do customers prefer?
+select * from order_payments;
+select * from order_items;
+
+select
+	op.payment_type,
+	COUNT(op.payment_type) as total_payments_per_type,
+	ROUND(COUNT(op.payment_type) * 100.0 /(select COUNT(*) from olist.order_payments)::numeric,1) as percentage_payments
+from olist.order_payments op 
+group by op.payment_type
+order by total_payments_per_type desc;
+
+--7. Reviews — What is the average review score by product category?
+select * from order_reviews r;
+select * from products;
+sele
+
